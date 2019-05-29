fedef23a9ca134753ac542465996a6b594af7f0a
Spidy20 <kushalbhavsar58@gmail.com>
2019-05-29 10:19:09 +0530

    new

--- spidy.py
+++ spidy.py
@@ -10,6 +10,7 @@ engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
 
+print('hello world ')
 
 def speak(audio):
     engine.say(audio)
